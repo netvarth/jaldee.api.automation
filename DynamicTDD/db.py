8d103be5d9a63b856ac89b836452f15d0a140049
Archana <archana.gopi@jaldee.com>
2023-08-25 16:33:58 +0530

    db.py

--- DynamicTDD/db.py
+++ DynamicTDD/db.py
@@ -40,7 +40,7 @@ import socket
 
 if os.environ['SYSTEM_ENV'] == 'Microsoft WSL':
     # db_host = "host.docker.internal"
-    db_host = "127.0.0.1"
+    db_host = socket.gethostbyaddr(socket.gethostname())[0]+".local"
 else:
     db_host = "127.0.0.1"
 
